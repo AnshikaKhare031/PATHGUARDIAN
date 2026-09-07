73f4de9056ee02c3f213a9f94e54fa63b03de680
Pratiti Maharana <pratitimaharana9@gmail.com>
2026-09-07 14:25:08 +0530

    Add POST endpoint for creating reports

--- database/api/main.py
+++ database/api/main.py
@@ -1,5 +1,11 @@
+from unicodedata import category
+
 from fastapi import FastAPI
-from database.geospatial.spatial_queries import get_score_for_point,  get_score_for_point
+from database.geospatial.spatial_queries import get_score_for_point,  get_reports_near, get_scores_for_points, insert_report
+from pydantic import BaseModel
+from typing import List, Tuple
+class RoutePoints(BaseModel):   #basemodel: used for defining the exact shape of the data 
+    points: List[Tuple[float, float]]
 app = FastAPI()
 
 
@@ -12,3 +18,29 @@ def read_root():
 def score(lat: float, lng: float):
     result = get_score_for_point(lat, lng)
     return {"lat": lat, "lng": lng, "score": result}
+
+@app.get("/api/reports/nearby")
+def reports_nearby(lat: float, lng: float, radius: float=1000):
+    results = get_reports_near(lat, lng, radius)
+    reports = [
+        {"id": r[0], "category": r[1], "description": r[2], "reported_at": str(r[3])}
+        for r in results
+    ]
+
+    return {"lat": lat, "lng": lng, "radius": radius, "reports": reports}
+
+@app.post("/api/scores/route")
+def scores_for_route(data: RoutePoints):
+    results = get_scores_for_points(data.points)
+    return {"scores": results}
+
+class ReportInput(BaseModel):
+    lat: float
+    lng: float
+    category: str
+    description: str
+
+@app.post("/api/reports")
+def create_report(data: ReportInput):
+    new_id = insert_report(data.lat, data.lng, data.category, data.description)
+    return {"message": "Report created", "id": new_id}
